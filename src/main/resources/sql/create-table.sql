828dd2863b0cef9ba66ccb53df2b32b7ff42b42b
NgoVanGiang <nvg28ngogiang@gmail.com>
2024-02-09 10:15:33 +0700

    Add corref_id and corref_position to create-table.sql

--- src/main/resources/sql/create-table.sql
+++ src/main/resources/sql/create-table.sql
@@ -45,7 +45,8 @@ create table amr_word
     word_label varchar(255),
     amr_label_id int,
     word_sense_id bigint
-    coref_word_id integer
+    corref_id bigint,
+    corref_position varchar(255)
 );
 
 create table word_sense
