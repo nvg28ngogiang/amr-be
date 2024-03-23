301bf1288b6ddc0fe860134925f6f8ebb45dbb8a
NgoVanGiang <nvg28ngogiang@gmail.com>
2024-03-23 10:46:15 +0700

    Attach AMR tree to sentence instead of user account

--- src/main/java/vn/edu/hus/amr/model/AmrTree.java
+++ src/main/java/vn/edu/hus/amr/model/AmrTree.java
@@ -14,9 +14,6 @@ public class AmrTree {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id")
-    private Long userId;
-
     @Column(name = "sentence_position")
     private String sentencePosition;
 
--- src/main/java/vn/edu/hus/amr/repository/AmrTreeRepository.java
+++ src/main/java/vn/edu/hus/amr/repository/AmrTreeRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 @Repository
 public interface AmrTreeRepository extends JpaRepository<AmrTree, Long> {
 
-    List<AmrTree> getByUserIdAndSentencePositionOrderById(Long userId, String sentencePosition);
+//    List<AmrTree> getByUserId(Long userId);
 
-    List<AmrTree> getByUserId(Long userId);
+    List<AmrTree> findBySentencePosition(String sentencePosition);
+
 }
